1d5b6ff9c855586d79f9e94626f3273ec17173f0
Ivan Font <ifont@redhat.com>
2023-03-03 12:24:15 -0800

    Log listening host:port info

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,10 @@ use wasmedge_wasi_socket::TcpListener;
 mod http;
 
 fn main() -> std::io::Result<()> {
+    let host = "0.0.0.0";
     let port = std::env::var("PORT").unwrap_or("8080".to_string());
-    println!("new connection at {}", port);
-    let listener = TcpListener::bind(format!("0.0.0.0:{}", port), false)?;
+    let listener = TcpListener::bind(format!("{}:{}", host, port), false)?;
+    println!("Now listening at {}:{}", host, port);
     loop {
         let _ = http::handle_client(listener.accept(false)?.0);
     }
